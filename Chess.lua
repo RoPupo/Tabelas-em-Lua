d0969d1d29bd17013816178482e439e33a4e5171
Rodrigo Pupo <109440284+RoPupo@users.noreply.github.com>
2024-09-05 17:46:13 -0300

    Teste gir revert

--- Chess.lua
+++ Chess.lua
@@ -9,9 +9,13 @@ board[4] = {}
 board[5] = {}
 board[6] = {}
 board[7] = {"♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎", "♟︎"}
-board[8] = {"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"}
+board[8] = {"♜", "♞", "♝", "", "♜"}
 
+
+-- _, -> são as os números [1] .. [2] são as linhas da primeira tabela
 for _, linha in pairs(board) do -- quando não tem uso a variavel por boa pratica ganha nome de _
+
+-- _, -> São as colunas da tabela. | tabuleiro
 local rowstring = ""
     for _, peca in pairs(linha)do
         rowstring = rowstring .. peca .. " "
